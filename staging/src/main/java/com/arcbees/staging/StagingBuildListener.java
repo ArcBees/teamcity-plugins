3edae6b6fbf14fae4342facde45df70abdacc241
Maxime Mériouma-Caron <maxime.meriouma@arcbees.com>
2014-07-29 14:58:36 -0400

    Using ExecutorService

--- staging/src/main/java/com/arcbees/staging/StagingBuildListener.java
+++ staging/src/main/java/com/arcbees/staging/StagingBuildListener.java
@@ -17,8 +17,11 @@
 package com.arcbees.staging;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.jetbrains.annotations.NotNull;
 
 import jetbrains.buildServer.buildTriggers.BuildTriggerDescriptor;
 import jetbrains.buildServer.serverSide.Branch;
@@ -26,45 +29,59 @@ import jetbrains.buildServer.serverSide.BuildServerAdapter;
 import jetbrains.buildServer.serverSide.BuildServerListener;
 import jetbrains.buildServer.serverSide.SBuildType;
 import jetbrains.buildServer.serverSide.SRunningBuild;
+import jetbrains.buildServer.serverSide.executors.ExecutorServices;
 import jetbrains.buildServer.util.EventDispatcher;
 
 public class StagingBuildListener {
     private static final Logger LOGGER = Logger.getLogger(StagingBuildListener.class.getName());
 
     private final TomcatDeployHandler deployHandler;
+    private final ExecutorService executorService;
 
     public StagingBuildListener(EventDispatcher<BuildServerListener> listener,
+                                ExecutorServices executorServices,
                                 TomcatDeployHandler deployHandler) {
         this.deployHandler = deployHandler;
+        executorService = executorServices.getLowPriorityExecutorService();
+
         listener.addListener(new BuildServerAdapter() {
             @Override
-            public void buildFinished(SRunningBuild build) {
-                try {
-                    onBuildFinished(build);
-                } catch (IOException e) {
-                    LOGGER.log(Level.SEVERE, "Error getting pull request infos", e);
-                }
+            public void buildFinished(@NotNull SRunningBuild build) {
+                onBuildFinished(build);
             }
         });
     }
 
-    private void onBuildFinished(SRunningBuild build) throws IOException {
+    private void onBuildFinished(final SRunningBuild build) {
         SBuildType buildType = build.getBuildType();
         if (buildType == null) {
             return;
         }
 
-        for (BuildTriggerDescriptor trigger : buildType.getResolvedSettings().getBuildTriggersCollection()) {
+        for (final BuildTriggerDescriptor trigger : buildType.getResolvedSettings().getBuildTriggersCollection()) {
             if (!trigger.getType().equals(TomcatStagingFeature.NAME)) {
                 continue;
             }
 
             Branch branch = build.getBranch();
             if (branch != null) {
-                deployHandler.handle(build, trigger);
+                handleDeploy(build, trigger);
             } else {
                 LOGGER.severe("Unknown branch name");
             }
         }
+    }
+
+    private void handleDeploy(final SRunningBuild build, final BuildTriggerDescriptor trigger) {
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    deployHandler.handle(build, trigger);
+                } catch (IOException e) {
+                    LOGGER.log(Level.SEVERE, "Error getting pull request infos", e);
+                }
+            }
+        });
     }
 }
